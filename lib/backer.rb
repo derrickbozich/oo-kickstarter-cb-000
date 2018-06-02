173e927a6ef201040b03eb8c9d259dbb4f6ed283
derrick b <dbozich1@gmail.com>
2018-06-02 07:26:47 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,18 @@
 class Backer
   attr_accessor :name
 
+  @@backed_projects = []
+
   def initialize(name)
     @name = name
+  end
+
+  def self.all
+    @@backed_projects
+  end
+
+  def back_project(project)
+    @@backed_projects << project
   end
 
 
