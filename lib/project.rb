cb140744cd96578f7f19fd8566af27c6e00437ad
derrick b <dbozich1@gmail.com>
2018-06-02 07:26:23 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,18 @@
 class Project
   attr_accessor :title
 
+  @@backers = []
+
+  def self.all
+    @@backers
+  end
+
   def initialize(title)
     @title = title
+  end
+
+  def add_backer(backer)
+    self.all.push(backer)
   end
 
 end
